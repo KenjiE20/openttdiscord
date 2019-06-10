ce10dbc44fdbb54e2b7b2ed8771f6fbcd32b1782
KenjiE20 <KenjiE20@yahoo.co.uk>
2019-06-10 13:54:31 +0000

    Fix newserver when config is missing section

--- commands/newserver.js
+++ commands/newserver.js
@@ -13,13 +13,17 @@ module.exports = {
         message.client.channelMap.set(message.channel.id, new OpenTTD.Client(args[0], args[1], args[2], null, message.channel));
         // Add element config to the main config
         let map = message.client.channelMap.get(message.channel.id);
+        // Make a channelMapping section if it doesn't exist yet
+        if (message.client.config.channelMapping === undefined) {
+            message.client.config.channelMapping = {};
+        }
         let mapconfig = message.client.config.channelMapping[message.channel.id] = {};
         mapconfig.name = map.name;
         mapconfig.address = map.address;
         mapconfig.port = map.port;
         mapconfig.password = map.password;
-        global.logger.info(`New OpenTTD server set up for channel:${message.channel.id}`);
-        message.reply('Config set up.');
+        global.logger.info(`New OpenTTD server set up for channel: ${message.channel.id}`);
+        message.reply('Config set up, remember to save the config file');
         global.logger.trace(`channelMapping:\n${JSON.stringify(message.client.config.channelMapping, null, 4)}`);
     }
 };
