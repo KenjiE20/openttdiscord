1db77651336e70f7fe7169f737adbb88412e7c8b
KenjiE20 <KenjiE20@yahoo.co.uk>
2019-06-13 15:31:08 +0000

    Use guild channel check

--- commands/newserver.js
+++ commands/newserver.js
@@ -5,10 +5,8 @@ module.exports = {
     name: 'newserver',
     description: 'Set up a new OpenTTD server for this channel, you can optionally provide a name, server address and port. For security the admin port password must be added via config.',
     usage: '<[name] [address] [port]>',
+    guildOnly: true,
     execute(message, args) {
-        if (message.channel.type !== 'text') {
-            return message.reply('This command must be run on a text channel');
-        }
         // Set up a new element in the channel map
         message.client.channelMap.set(message.channel.id, new OpenTTD.Client(args[0], args[1], args[2], null, message.channel));
         // Add element config to the main config
