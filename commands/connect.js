a48b79af7a1811ef3b5f37f0b7fc06b757df417d
KenjiE20 <KenjiE20@yahoo.co.uk>
2019-06-13 15:34:15 +0000

    Add connect command

---
+++ commands/connect.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'connect',
+    description: 'Connect to the OpenTTD server mapped to this channel',
+    guildOnly: true,
+    openttd: true,
+    execute(message) {
+        // Get the OpenTTD server for the channel
+        const openttd = message.client.channelMap.get(message.channel.id);
+
+        // Attempt to connect
+        if (openttd.isConnected) {
+            return message.reply('Already connected');
+        } else {
+            return openttd.connect();
+        }
+    }
+};
