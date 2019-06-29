91664821e0618dca9f5e08d56239b2fa6c4a8d8a
Kenji Eva <KenjiE20@yahoo.co.uk>
2019-06-29 16:51:09 +0000

    Add pm2 aware stop to shutdown command

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -5,7 +5,21 @@ module.exports = {
     execute(message) {
         global.logger.debug(`Message author id: ${message.author.id} | OwnerID: ${message.client.config.ownerID}`);
         if (message.author.id === message.client.config.ownerID) {
-            message.client.botShutdown();
+            // pm2 existence check
+            if (process.env.pm_id) {
+                const pm2 = require('pm2');
+                global.logger.debug(`Using pm2 to stop ${process.env.name}`);
+                pm2.connect(err => {
+                    if (err) global.logger.error(`Error with pm2 connect: ${err}`);
+                });
+                pm2.stop(process.env.name, err => {
+                    if (err) global.logger.error(`Error with pm2 stop: ${err}`);
+                });
+                pm2.disconnect();
+                global.logger.debug('Finished with pm2');
+            } else {
+                message.client.botShutdown();
+            }
         }
     }
 };
