7b7ef52d8a97a0ee989cb9627131086924f24fb0
Kenji Eva <KenjiE20@yahoo.co.uk>
2019-07-08 17:33:15 +0000

    Password command now checks for updates

--- commands/password.js
+++ commands/password.js
@@ -10,35 +10,90 @@ module.exports = {
 
         // Check connection
         if (openttd.isConnected) {
-            const command = 'setting server_password';
+            const RCONCMD = 'setting server_password';
+            // Password string
+            let password;
+            // Update check count
+            let passwordCheckCount = 0;
+            // Sent bot message for edits
+            let pwMessage;
+
+            // Output or update message on channel, takes password as input
+            const doPasswordMessage = function(pwd) {
+                // Build text for discord
+                let passwordText;
+                if (pwd) {
+                    passwordText = `\`Current password: ${pwd}\``;
+                } else {
+                    passwordText = '`There is no password for this server`';
+                }
+                // If we've previously output
+                if (pwMessage) {
+                    // Edit to update password
+                    pwMessage.edit(passwordText)
+                        .then(() => {
+                            // Update occured, stop checks
+                            global.logger.debug('Password updated, ending update checks');
+                            clearInterval(PASSWORDTIMER);
+                        })
+                        .catch(global.logger.error);
+                } else {
+                    // Send new reply
+                    message.channel.send(passwordText)
+                        .then((sentMes) => {
+                            // Output returned Message back
+                            pwMessage = sentMes;
+                        })
+                        .catch(global.logger.error);
+                }
+            };
 
             // rcon callback
             const parser = function(rcon) {
                 global.logger.trace('got rcon:', rcon);
+                // regexp search and store
                 const pw = /Current value for 'server_password' is: '(.*)'/.exec(rcon.output);
                 global.logger.trace('pw', pw);
-                if (pw[1]) {
-                    message.channel.send(`\`Current password: ${pw[1]}\``);
-                } else {
-                    message.channel.send('`There is no password for this server`');
+                global.logger.trace('password', password);
+
+                // Do nothing if passwords match
+                if (pw[1] === password) {
+                    return;
                 }
+
+                password = pw[1];
+                doPasswordMessage(password);
             };
 
             // rconend callback
-            const disconnect = function(rconend) {
+            const end = function(rconend) {
                 global.logger.trace('Got rconend:', rconend);
-                if (rconend.command === command) {
+                if (rconend.command === RCONCMD) {
+                    // Bump counter and remove open event binds
+                    passwordCheckCount += 1;
                     openttd.connection.removeListener('rcon', parser);
-                    openttd.connection.removeListener('rconend', disconnect);
+                    openttd.connection.removeListener('rconend', end);
+
+                    if (passwordCheckCount >= 7) {
+                        global.logger.debug('Password not updated within 30secs, ending update checks');
+                        clearInterval(PASSWORDTIMER);
+                    }
                 }
             };
 
-            // Bind to rcon
-            openttd.connection.on('rcon', parser);
-            openttd.connection.on('rconend', disconnect);
+            // Start password check
+            const doPasswordCheck = function() {
+                // Bind to rcon
+                openttd.connection.on('rcon', parser);
+                openttd.connection.on('rconend', end);
 
-            // Send rcon
-            openttd.connection.send_rcon(command);
+                // Send rcon
+                openttd.connection.send_rcon(RCONCMD);
+            };
+
+            // Call check then schedule checks every 5 secs
+            doPasswordCheck();
+            const PASSWORDTIMER = setInterval(doPasswordCheck, 5000);
         } else {
             message.reply('Not connected');
         }
