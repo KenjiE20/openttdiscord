47df5eb6a2aa6ec03f63825f8f0a988192df9dfe
Kenji Eva <KenjiE20@yahoo.co.uk>
2019-10-04 17:22:17 +0000

    Fix newserver command
    Make the newserver command work with the new OpenTTD Client object

--- commands/newserver.js
+++ commands/newserver.js
@@ -15,20 +15,31 @@ module.exports = {
             return;
         }
 
-        // Set up a new element in the channel map
-        message.client.channelMap.set(message.channel.id, new OpenTTD.Client(args[0], args[1], args[2], null, message.channel));
-        // Add element config to the main config
-        const map = message.client.channelMap.get(message.channel.id);
         // Make a channelMapping section if it doesn't exist yet
         if (message.client.config.channelMapping === undefined) {
             message.client.config.channelMapping = {};
         }
+
+        // Create config section with args if given
         const mapConfig = message.client.config.channelMapping[message.channel.id] = {};
+        if (args) {
+            mapConfig.name = args[0];
+            mapConfig.address = args[1];
+            mapConfig.port = args[2];
+        }
+
+        // Set up a new element in the channel map
+        message.client.channelMap.set(message.channel.id, new OpenTTD.Client(mapConfig, message.channel));
+        // Add element config to the main config
+        const map = message.client.channelMap.get(message.channel.id);
+
+        // Copy elements back to config
         mapConfig.name = map.name;
         mapConfig.address = map.address;
         mapConfig.port = map.port;
         mapConfig.password = map.password;
         mapConfig.autoconnect = map.autoconnect;
+        mapConfig.public = map.publicAddress;
         global.logger.info(`New OpenTTD server set up for channel: ${message.channel.id}`);
         message.reply('Config set up, remember to save the config file');
         global.logger.trace('channelMapping:', message.client.config.channelMapping);
